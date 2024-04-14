944138c7d5a4b44dfe8f5016761d4ec7482a391e
Venky Venkatakrishnan <eeshwar1@gmail.com>
2024-04-14 17:06:34 -0500

    UI Changes and NSColor compare

--- ImageGen/ImageGen.swift
+++ ImageGen/ImageGen.swift
@@ -7,22 +7,45 @@
 
 import Cocoa
 
+enum ShapeType: String {
+    
+    case Squares = "Squares"
+    case VerticalBars = "Vertical Bars"
+    case HorizontalBars = "Horizontal Bars"
+    
+}
+
 class ImageGen {
     
-   
-    static func generate(size: NSSize, colors: [NSColor], fill: Bool) -> NSImage {
+    static func generate(shapeType: ShapeType, size: NSSize, shapeFactor: CGFloat, backgroundColor: NSColor, colors: [NSColor], random: Bool, fill: Bool) -> NSImage {
         
+        switch shapeType {
+            
+        case .Squares:
+            return generateSquares(size: .init(width: size.width, height: size.height), shapeFactor: 50, backgroundColor: NSColor.white, colors: colors, random: random, fill: fill)
+        case .VerticalBars:
+            return generateBars(size: .init(width: size.width, height: size.height), shapeFactor: 50, backgroundColor: NSColor.white, colors: colors, vertical: true, random: random, fill: fill)
+        case .HorizontalBars:
+            return generateBars(size: .init(width: size.width, height: size.height), shapeFactor: 50, backgroundColor: NSColor.white, colors: colors, vertical: false, random: random, fill: fill)
+            
+        }
+    }
+    
+    static func generateSquares(size: NSSize, shapeFactor: CGFloat, backgroundColor: NSColor, colors: [NSColor], random: Bool, fill: Bool) -> NSImage {
+        
+        guard colors.count > 0 else { return NSImage(size: size) }
         
         return NSImage(size: size,
                                   flipped: false,
                                   drawingHandler: { rect in
-            NSColor.gray.set()
+            
+            backgroundColor.set()
             rect.fill()
             
-            let shapeWidth = CGFloat(size.width/10)
-            let shapeHeight = CGFloat(size.width/10)
+            let shapeWidth = CGFloat(size.width/shapeFactor)
+            let shapeHeight = CGFloat(size.width/shapeFactor)
 
-            let spacing = CGFloat(size.width/25)
+            let spacing = CGFloat(size.width/(2.5 * shapeFactor))
             var xOffset: CGFloat = spacing
             var yOffset: CGFloat = spacing
             var xPos: CGFloat = 0.0
@@ -37,14 +60,14 @@ class ImageGen {
                 
                 yPos = CGFloat(ridx) * shapeHeight + yOffset
                 xOffset = spacing
-                if CGFloat(yPos) + shapeHeight < size.height
+                if CGFloat(yPos) + shapeHeight <= size.height
                     
                 {
                     for cidx in 0...colsOfShapes {
                         
                         xPos = CGFloat(cidx) * shapeWidth + xOffset
                         
-                        if CGFloat(xPos) + shapeWidth < size.width {
+                        if CGFloat(xPos) + shapeWidth <= size.width {
                             
                             let smallRect = NSBezierPath(rect: NSRect(x: xPos, y: yPos, width: shapeWidth, height: shapeHeight))
                             xOffset += spacing
@@ -62,12 +85,18 @@ class ImageGen {
                             }
                           
                             
-                            colorIdx += 1
-                            
-                            if colorIdx >= colors.count {
+                            if random {
                                 
-                                colorIdx = 0
-                            
+                                colorIdx = Int.random(in: 0..<colors.count)
+                                
+                            } else {
+                                colorIdx += 1
+                                
+                                if colorIdx >= colors.count {
+                                    
+                                    colorIdx = 0
+                                    
+                                }
                             }
                             
                         }
@@ -82,5 +111,162 @@ class ImageGen {
         
     }
     
+    static func generateBars(size: NSSize, shapeFactor: CGFloat, backgroundColor: NSColor, colors: [NSColor], vertical: Bool, random: Bool, fill: Bool) -> NSImage {
+    
+        guard colors.count > 0 else { return NSImage(size: size) }
+        
+        
+        if vertical {
+            
+            return generateVerticalBars(size: size, shapeFactor: shapeFactor, backgroundColor: backgroundColor, colors: colors, random: random, fill: fill)
+            
+        } else {
+            
+            return generateHorizontalBars(size: size, shapeFactor: shapeFactor, backgroundColor: backgroundColor, colors: colors, random: random, fill: fill)
+        }
+    }
+    
+  
+    
+    static func generateVerticalBars(size: NSSize, shapeFactor: CGFloat, backgroundColor: NSColor, colors: [NSColor], random: Bool, fill: Bool) -> NSImage {
+        
+        guard colors.count > 0 else { return NSImage(size: size) }
+        
+        
+        return NSImage(size: size,
+                                  flipped: false,
+                                  drawingHandler: { rect in
+            
+            backgroundColor.set()
+            rect.fill()
+            
+            let shapeWidth = CGFloat(size.width/shapeFactor)
+            let shapeHeight = size.height
+        
+            let spacing = 0.0
+            var xOffset: CGFloat = 0.0
+            var xPos: CGFloat = 0.0
+            
+            let colsOfShapes = Int(size.width/(shapeWidth + spacing))
+            
+            var colorIdx = 0
+            
+         
+                for cidx in 0...colsOfShapes {
+                    
+                    xPos = CGFloat(cidx) * shapeWidth + xOffset
+                    
+                    if CGFloat(xPos) + shapeWidth <= size.width {
+                        
+                        let smallRect = NSBezierPath(rect: NSRect(x: xPos, y: 0, width: shapeWidth, height: shapeHeight))
+                        xOffset += spacing
+                        smallRect.lineWidth = 2
+                        
+                        if fill {
+                            
+                            colors[colorIdx].set()
+                            smallRect.fill()
+                            
+                        } else {
+                            
+                            colors[colorIdx].setStroke()
+                            smallRect.stroke()
+                        }
+                      
+                        
+                        if random {
+                            
+                            colorIdx = Int.random(in: 0..<colors.count)
+                            
+                        } else {
+                            colorIdx += 1
+                            
+                            if colorIdx >= colors.count {
+                                
+                                colorIdx = 0
+                                
+                            }
+                        }
+                        
+                    }
+                }
+               
+            
+           
+            return true
+            
+        })
+        
+    }
+    
+    
+    static func generateHorizontalBars(size: NSSize, shapeFactor: CGFloat, backgroundColor: NSColor, colors: [NSColor], random: Bool, fill: Bool) -> NSImage {
+        
+        
+        return NSImage(size: size,
+                                  flipped: false,
+                                  drawingHandler: { rect in
+            
+            backgroundColor.set()
+            rect.fill()
+            
+            let shapeWidth = size.width
+            let shapeHeight = CGFloat(size.height/shapeFactor)
+        
+            let spacing = 0.0
+            var yOffset: CGFloat = 0.0
+            var yPos: CGFloat = 0.0
+            
+            let rowsOfShapes = Int(size.height/(shapeHeight + spacing))
+            
+            var colorIdx = 0
+            
+         
+                for cidx in 0...rowsOfShapes {
+                    
+                    yPos = CGFloat(cidx) * shapeHeight + yOffset
+                    
+                    if CGFloat(yPos) + shapeHeight <= size.height {
+                        
+                        let smallRect = NSBezierPath(rect: NSRect(x: 0, y: yPos, width: shapeWidth, height: shapeHeight))
+                        yOffset += spacing
+                        smallRect.lineWidth = 2
+                        
+                        if fill {
+                            
+                            colors[colorIdx].set()
+                            smallRect.fill()
+                            
+                        } else {
+                            
+                            colors[colorIdx].setStroke()
+                            smallRect.stroke()
+                        }
+                      
+                        
+                        if random {
+                            
+                            colorIdx = Int.random(in: 0..<colors.count)
+                            
+                        } else {
+                            colorIdx += 1
+                            
+                            if colorIdx >= colors.count {
+                                
+                                colorIdx = 0
+                                
+                            }
+                        }
+                        
+                    }
+                }
+               
+            
+           
+            return true
+            
+        })
+        
+    }
 
 }
--- ImageGen/NSColor+Extension.swift
+++ ImageGen/NSColor+Extension.swift
@@ -5,4 +5,43 @@
 //  Created by Venkateswaran Venkatakrishnan on 4/14/24.
 //
 
-import Foundation
+import Cocoa
+
+extension NSColor {
+    
+    func RGB() -> String {
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        // minimum decimal digit, eg: to display 2 as 2.00
+        formatter.minimumFractionDigits = 2
+        // maximum decimal digit, eg: to display 2.5021 as 2.50
+        formatter.maximumFractionDigits = 2
+        // round up 21.586 to 21.59. But doesn't round up 21.582, making it 21.58
+        formatter.roundingMode = .halfUp
+        
+        let redString = formatter.string(for: self.redComponent)!
+        let greenString = formatter.string(for: self.greenComponent)!
+        let blueString = formatter.string(for: self.blueComponent)!
+        let alphaString = formatter.string(for: self.alphaComponent)!
+            
+        return "(\(redString),\(greenString),\(blueString),\(alphaString))"
+    }
+    
+    func isSimilar(to otherColor: NSColor) -> Bool {
+        
+        let redDifference = self.redComponent - otherColor.redComponent
+        let greenDifference = self.greenComponent - otherColor.greenComponent
+        let blueDifference = self.blueComponent - otherColor.blueComponent
+        let alphaDifference = self.alphaComponent - otherColor.alphaComponent
+        
+        let colorDifference: Double = (pow(redDifference, 2) + pow(greenDifference,2) + pow(blueDifference, 2) + pow(alphaDifference, 2)).squareRoot()
+        
+        if colorDifference <= 0.1 {
+            
+            return true
+        }
+        
+        return false
+    }
+}
